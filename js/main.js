2965a51e2a1235e69e3081192fddfb042cab32c1
EshimovShaxzod <126951571+EshimovShaxzod@users.noreply.github.com>
2024-02-05 19:09:25 +0500

    movies app done

--- js/main.js
+++ js/main.js
@@ -8,6 +8,25 @@ let API_KEY = '310c3424'
 
 let API = `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}`
 
+// IMDBId   movie function
+function generateMovieImdbLink(imdbId){
+    return `https://www.imdb.com/title/${imdbId}/`
+}
+
+function generateYoutubeLink(FoundMovieYtId){
+    return `https://www.youtube.com/embed/${FoundMovieYtId}`
+ }
+
+// *************************************************************
+
+let elModal = document.querySelector('.modal')
+let elMovieImg = document.querySelector('.movie-img')
+let elModalTitle = document.querySelector('.modal-movie-title');
+let elModalIframe = document.querySelector('.modal-movie-iframe');
+let elModalRating = document.querySelector('.modal-movie-rating');
+let elModalYear = document.querySelector('.modal-movie-year');
+let elModalLink = document.querySelector('.modal-movie-link');
+
 let renderMovies = (search = 'iron') => {
 
     elMovieResult.innerHTML = null
@@ -15,19 +34,23 @@ let renderMovies = (search = 'iron') => {
     fetch(API + `&s=${search}`)
     .then(res => res.json())
     .then(data => data.Search.forEach((movie) => {
-
-        console.log(data);
+    //  console.log(data);
+        // console.log(data);
         let cloneMovies = elMovieTemplate.cloneNode(true)
 
         cloneMovies.querySelector('.movie-img').src = movie.Poster
         cloneMovies.querySelector('.movie-title').textContent = movie.Title
         cloneMovies.querySelector('.movie-type').textContent = movie.Type
         cloneMovies.querySelector('.movie-year').textContent = movie.Year
+        cloneMovies.querySelector('.info-btn').dataset.movieId = movie.imdbID
 
         movieFragment.appendChild(cloneMovies)
         
         elMovieResult.appendChild(movieFragment)
-    }))
+
+       
+
+    },  showMovie(data.Search)))
 
 }
 
@@ -44,5 +67,35 @@ let inputValue = elSearchInput.value.trim();
 renderMovies(inputValue)
 
 })
+
+function showMovie(movie) {
+
+elMovieResult.addEventListener('click', (evt) => {
+
+    let clickId =evt.target.dataset.movieId;
+    
+    
+        if(evt.target.matches('.info-btn')){
+            
+            let movieFilter = movie.filter((movie) => {
+                return clickId === movie.imdbID
+            })
+
+            movieFilter.forEach((m) => {
+
+             elModalTitle.textContent = m.Title;
+             elMovieImg.src = m.Poster;
+             elModalLink.href = generateMovieImdbLink(m.imdbID);
+             elModalYear.textContent = m.Year;
+             elModalRating.textContent = m.Type
+
+                
+            })
+            
+
+        }
+});
+}
+
 
 
